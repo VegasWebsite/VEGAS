a87929c10ba72cb5005151ea2f9817cd62e46659
limjonghyeok <jjognbbang2@naver.com>
2024-03-10 11:34:21 +0900

    hight 조절

--- src/mainPage/mainPageJS/MainBanner.js
+++ src/mainPage/mainPageJS/MainBanner.js
@@ -1,11 +1,9 @@
 import React from "react";
 import "../mainPageCSS/mainPageBanner.css"
-import Banner from "../mainPageIMG/mainbackground.svg";
 
 function MainBanner(){
     return(
-        <div id="mainBanner" loading="lazy">
-            <img src={Banner}/>
+        <div id="mainBanner">
         </div>
     )
 }
