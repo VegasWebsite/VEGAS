5d31096ad76f7b78a59965b3f48237589605dbec
limjonghyeok <jjognbbang2@naver.com>
2024-03-10 18:46:29 +0900

    companyPage

--- src/companyPage/companyPageJS/CompanyBanner.js
+++ src/companyPage/companyPageJS/CompanyBanner.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "../companyPageCSS/companyBanner.css"
-import Banner from "../companyPageIMG/companyBanner.svg";
 
 function CompanyBanner(){
     return(
-        <div id="companyBanner">
-            <img id="banner" src={Banner}/> 
+        <div id="Banner">
+
         </div>
     )
 }
