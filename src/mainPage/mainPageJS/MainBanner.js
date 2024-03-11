0953adf8756b3d0d1813bad2a65a45cbda2ea6cd
limjonghyeok <jjognbbang2@naver.com>
2024-03-11 21:42:58 +0900

    main Banner

--- src/mainPage/mainPageJS/MainBanner.js
+++ src/mainPage/mainPageJS/MainBanner.js
@@ -1,8 +1,11 @@
 import React from "react";
-import "../mainPageCSS/mainPageBanner.css"
+import "../mainPageCSS/mainPageBanner.css";
+import Banner from "../mainPageIMG/mainbackground.svg";
+
 function MainBanner(){
     return(
-        <div id="mainBanner">
+        <div id="banner">
+            <img src = {Banner}/>
         </div>
     )
 }
