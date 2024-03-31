8644a10555c5c06e2728d93c602a25bbecaab9e6
limjonghyeok <jjognbbang2@naver.com>
2024-03-31 15:52:38 +0900

    company mobile

--- src/companyPage/companyPageJS/CompanyPage.js
+++ src/companyPage/companyPageJS/CompanyPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../companyPageCSS/companyPage.css"
 
 
@@ -9,16 +9,35 @@ import CompanyBanner from "./CompanyBanner";
 import CompanyOverviewSelect from "./CompanyOverviewSelect";
 import CompanyWaytoBackground from "./CompanyWaytoBackground";
 
-import ComponyOverview from "../companyPageIMG/companyOverview.svg";
+import CompanyOverview from "../companyPageIMG/companyOverview.svg";
 import Ceo from "../companyPageIMG/ceo.svg";
 import OrganizationChart from "../companyPageIMG/organizationChart.svg";
+import MCompanyOverview from "../companyPageIMG/mobileIMG/mCompanyOverview.svg"
+import MCEO from "../companyPageIMG/mobileIMG/mCEO.svg"
+import MOrganizationChart from "../companyPageIMG/mobileIMG/mOrganizationChart.svg"
+
+
 function CompanyPage() {
 
+  
     const [activeImage, setActiveImage] = useState(0);
+  
+
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenWidth(window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     const childData = (data) => {
         showImage(data);
     }
-
 
     const showImage = (index) =>{
         setActiveImage(index);
@@ -36,12 +55,25 @@ function CompanyPage() {
              </div>
 
             <div id="companyOverview">
-                <img src={ComponyOverview} className={activeImage == 0 ? 'active' : 'image'}/>
-                <img src={Ceo} className={activeImage == 1 ? 'active' : 'image'}/>
-                <img src={OrganizationChart} className={activeImage == 2 ? 'active' : 'image'}/>
-                <div id="companyWaytoBackground"  className={activeImage == 3 ? 'active' : 'image'}>
-                    <CompanyWaytoBackground activeImage = {activeImage}/>
-                </div>
+                {(screenWidth <= 900) ? (
+                    <>
+                        <img src={MCompanyOverview} className={activeImage === 0 ? 'active' : 'image'} />
+                        <img src={MCEO} className={activeImage === 1 ? 'active' : 'image'} />
+                        <img src={MOrganizationChart} className={activeImage === 2 ? 'active' : 'image'} />
+                        <div id="companyWaytoBackground" className={activeImage === 3 ? 'active' : 'image'}>
+                            <CompanyWaytoBackground activeImage={activeImage} />
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <img src={CompanyOverview} className={activeImage === 0 ? 'active' : 'image'} />
+                        <img src={Ceo} className={activeImage === 1 ? 'active' : 'image'} />
+                        <img src={OrganizationChart} className={activeImage === 2 ? 'active' : 'image'} />
+                        <div id="companyWaytoBackground" className={activeImage === 3 ? 'active' : 'image'}>
+                            <CompanyWaytoBackground activeImage={activeImage} />
+                        </div>
+                    </>
+                )}
             </div>
             <div id="companyFooter">
                     <CommonFooter/>
--- src/companyPage/companyPageJS/CompanyWaytoBackground.js
+++ src/companyPage/companyPageJS/CompanyWaytoBackground.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import "../companyPageCSS/companyWayBackground.css";
 import WaytoBackground from "../companyPageIMG/waytoBackground.svg";
+import MWaytoBackGround from "../companyPageIMG/mobileIMG/mWaytoBackground.svg"
 import HeadOfficeMap from "./HeadOfficeMap";
 import FactoryMap from "./FactoryMap";
 
 function CompanyWaytoBackground(props){
+
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenWidth(window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+    
     return(
         <div id="companyWayBackground">
-            <img src={WaytoBackground} />
+            {(screenWidth <= 900) ? (
+                <>
+                    <img src={MWaytoBackGround} />
+                </>
+            ) : (
+                <>
+                    <img src={WaytoBackground} />
+                </>
+            )}
             <div id="companyWay">
                 <div id="companyMaps">
                     <div id="companyMap1">
@@ -18,8 +39,8 @@ function CompanyWaytoBackground(props){
                         </div>
                         <p>경기도 성남시 중원구 사가막골 62번길 33(성남 센터엠지식산업센터9층),904호</p>
                         <p>13211</p>
-
                     </div>
+
                     <div id="companyMap2">
                         <h4>공장</h4>
                             
@@ -28,7 +49,6 @@ function CompanyWaytoBackground(props){
                         </div>
                         <p>경기 광주시 곤지암을 곤지암리 108</p>
                         <p>12801</p>   
-                        
                     </div>
 
                 </div>
